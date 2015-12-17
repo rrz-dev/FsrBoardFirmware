92cadd7041aef267557ad23f3a31e871597c8fe3
Glatzemann <Glatzemann@email.de>
2015-12-17 14:20:47 +0100

    fixed GCode-Parser issue (non ending command state)

--- GCodeParser.cpp
+++ GCodeParser.cpp
@@ -74,6 +74,7 @@ void GCodeParser::parse(const char& c, AddCommandCallback addCommand)
           {
             addCommand(*command);
             command->setCommandType(UNKNOWN);
+            Serial.println("End command 1");
           }
           break;
       }
@@ -97,6 +98,7 @@ void GCodeParser::parse(const char& c, AddCommandCallback addCommand)
           {
             addCommand(*command);
             command->reset();
+            changeState(None);
           }
           break;
         case 'x':   // A X coordinate, usually to move to. This can be an Integer or Fractional number. 
