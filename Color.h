0cb8556eca178cf8df7a93451069cc26a3fe48c0
Glatzemann <Glatzemann@outlook.de>
2015-12-21 09:41:58 +0100

    implemented HSV color interpolation and linear interpolator

--- Color.h
+++ Color.h
@@ -34,6 +34,11 @@ struct HSV
   float v;
 };
 
+int linearF(float a, float b, float t)
+{
+    return a * (1.0f - t) + b * t;
+}
+
 class Color
 {
 public:
@@ -43,6 +48,20 @@ public:
 
   RGB getRGB() { return rgb; }
   HSV getHSV() { return hsv; }
+
+  template<typename F>
+  Color interpolate(Color b, float t, F interpolator)
+  {
+    // 0.0 <= t <= 1.0
+    HSV hsvB  = b.getHSV();
+    HSV final;
+
+    final.h = interpolator(hsv.h, hsvB.h, t);
+    final.s = interpolator(hsv.s, hsvB.s, t);
+    final.v = interpolator(hsv.v, hsvB.v, t);
+
+    return Color(final);
+  }
 
 private:
   void calcHsv();
